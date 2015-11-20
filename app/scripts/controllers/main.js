3a8fbcac9c44a6fada4b769031826197ab324954
Grant McDonald <grantmcdonald@Grants-MacBook-Air.local>
2015-11-20 17:31:53 -0600

    added new content
    -todo project
    -PDM scheduler

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -30,8 +30,13 @@ angular.module('grantmcdApp')
       $scope.projects = [{
         name: "Dskus",
         description: "An app to allow communication between an individual presenting and their audience. Users are able to create or join \"discussions\" where they can answer polls or questions from the presenter. Dskus uses Parse for data storage and PubNub for real time communication between users. It's built with AngularJS. I plan on open sourcing this once I've made more progress.",
-        linkText: "Alpha Web App",
-        link: "http://dskus.parseapp.com/#/main"
+        linkText: "Beta Web App",
+        link: "http://dskus.parseapp.com/#/"
+      }, {
+        name: "Task Manager",
+        description: "I started working on this app because I was tired of looking for a dead simple task manager that syncs across all devices. It's a work in progress and I'm still thinking of a clever name for the app. Once the web app is finished, the plan is to build a mobile version for Android as well.",
+        linkText: "Beta Web App",
+        link: "http://todomcd.parseapp.com"
       }, {
         name: "Steam Crawler",
         description: "My senior design project requires a large amount of Steam game data which isn't available via an API, so I'm currently building an HTML crawler using jSoup to gather as much data as possible from the games' store pages.",
@@ -48,13 +53,20 @@ angular.module('grantmcdApp')
         name: "Steam User Graph",
         description: "This simple app was made during the research phase of my senior design project. My goals when working on this app were to become familiar with Steam's public API as well as to use the Sigma.js library for displaying graphs in the canvas element. Because Steam's API does not allow CORS, you will need to disable security in your browser for the app to function.",
         linkText: "Web App",
-        link: "http://may1635.sd.ece.iastate.edu/#/experiment",
+        link: "http://may1635.sd.ece.iastate.edu/#/graph",
         image: "steam-graph.png"
+      },
+      {
+        name: "PDM Schedule Creator",
+        description: "This tool was built for a class project, but it could be useful to others planning out their projects. The web app allows users to create tasks with a duration in weeks and assign any dependent tasks. Once finished, a report will be generated allowing a user to see the order of their tasks and when they need to be started and finished by. It also was a good opportunity to try out the HTML5 FileReader API for the first time.",
+        linkText: "Web App",
+        link: "/#/329-assignment4",
+        image: ""
       }];
 
       $scope.chosenExperiment = $scope.experiments[0];
 
-      $scope.bioText = "<b>Hello!</b> Welcome to my little corner of the web. I'm currently in my senior year studying Software Engineering at Iowa State University. At the moment, most of my work has been with web development on a variety of projects. Feel free to check out some of my git repos below. If you have any questions or just want to say hi, <a href='mailto:grantmcd@outlook.com'>shoot me an email</a>.";
+      $scope.bioText = "<b>Hello!</b> Welcome to my little corner of the web. I'm currently in my senior year studying Software Engineering at Iowa State University. At the moment, most of my work has been with web development on a variety of projects. Feel free to check out some of them below. If you have any questions or just want to say hi, <a href='mailto:grantmcd@outlook.com'>shoot me an email</a>.";
       $scope.subTitleText = "Software Developer, Web Designer, Student";
 
       $scope.chooseExperiment = function(experiment) {
